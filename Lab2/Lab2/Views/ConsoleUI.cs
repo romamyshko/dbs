e7af2d658f2afcca5389c903221b0fb8948ee547
Roman Myshko <romamyshko12@gmail.com>
2021-11-13 09:44:35 +0200

    move methods to courseUI

--- Lab2/Lab2/Views/ConsoleUI.cs
+++ Lab2/Lab2/Views/ConsoleUI.cs
@@ -15,6 +15,7 @@ namespace Lab2.Views
         private readonly DbConnectionInfo _dbConnectionInfo;
         private readonly CourseController _courseController;
         private readonly LectureController _lectureController;
+        private readonly CourseUI _courseUI;
 
         public ConsoleUI(string databaseConnection)
         {
@@ -23,6 +24,7 @@ namespace Lab2.Views
             _courseDBContext = new CourseDBContext(_dbConnectionInfo);
             _courseController = new CourseController(_courseDBContext);
             _lectureController = new LectureController(_courseDBContext);
+            _courseUI = new CourseUI(_courseController);
         }
 
         public void Run()
@@ -30,12 +32,11 @@ namespace Lab2.Views
             while (true)
             {
                 Console.Clear();
-
                 Console.WriteLine("Choose working option:\r\n[1] search in the database\r\n[2] make changes in the database\r\n\r\nwrite \"exit\" to exit the program");
 
                 try
                 {
-                    int option = GetOption();
+                    int option = GetIntInput();
 
                     switch (option)
                     {
@@ -53,12 +54,10 @@ namespace Lab2.Views
                 {
                     break;
                 }
-                
-                Console.Clear();
             }
         }
 
-        private int GetOption()
+        internal static int GetIntInput()
         {
             string entered = Console.ReadLine();
 
@@ -73,7 +72,6 @@ namespace Lab2.Views
             }
 
             int option;
-
             bool isInt = int.TryParse(entered, out option);
 
             if (!isInt)
@@ -95,17 +93,16 @@ namespace Lab2.Views
             while (true)
             {
                 Console.Clear();
-
                 Console.WriteLine("Choose table:\r\n[1] courses\r\n[2] lectures\r\n\r\n write \"back\" to return to the main menu");
 
                 try
                 {
-                    int option = GetOption();
+                    int option = GetIntInput();
 
                     switch (option)
                     {
                         case 1:
-                            OperationsWithCourses();
+                            _courseUI.OperationsWithCourses();
                             break;
                         case 2:
                             OperationsWithLectures();
@@ -118,117 +115,10 @@ namespace Lab2.Views
                 {
                     break;
                 }
-
-                Console.Clear();
             }
         }
 
-        private void OperationsWithCourses()
-        {
-            while (true)
-            {
-                Console.Clear();
-
-                Console.WriteLine("Choose operation:\r\n[1] create\r\n[2] edit\r\n[3] delete\r\n\r\n write \"back\" to step back");
-
-                try
-                {
-                    int option = GetOption();
-
-                    switch (option)
-                    {
-                        case 1:
-                            CreateCourse();
-                            break;
-                        case 2:
-                            EditCourse();
-                            break;
-                        case 3:
-                            DeleteCourse();
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                catch
-                {
-                    break;
-                }
-                
-                Console.Clear();
-            }
-        }
-
-        private void CreateCourse()
-        {
-            while (true)
-            {
-                Console.Clear();
-
-                Console.WriteLine("Write name of course\r\n\r\n write \"back\" to step back");
-
-                Course course = new Course();   
-                string name = Console.ReadLine();
-
-                if (name.Equals("back"))
-                    break;
-
-                course.Name = name;
-
-                string cost;
-                int intCost = -1;
-
-                while (true)
-                {
-                    Console.Clear();
-
-                    Console.WriteLine("Write cost of course\r\n\r\n write \"back\" to step back");
-
-                    cost = Console.ReadLine();
-
-                    if (cost.Equals("back"))
-                        break;
-
-                    bool isInt = int.TryParse(cost, out intCost);
-
-                    if (!isInt)
-                    {
-                        Console.Clear();
-                        continue;
-                    }
-
-                    break;
-                }
-
-                if (intCost == -1)
-                    break;
-
-                course.Cost = intCost;
-                course.CreatedAt = DateTime.Now;
-
-                
-
-                if (_courseController.Create(course) == 1)
-                {
-                    Console.WriteLine("Operation is successfull. Press any key to continue...");
-                    if (Console.ReadKey().Equals(new ConsoleKeyInfo()))
-                        return;
-                }
-
-
-                return;
-            }
-        }
-
-        private void EditCourse()
-        {
-
-        }
-
-        private void DeleteCourse()
-        {
-
-        }
+        
 
         private void OperationsWithLectures()
         {
---
+++ Lab2/Lab2/Views/CourseUI.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab2.Models;
+using Lab2.Controllers;
+
+namespace Lab2.Views
+{
+    internal class CourseUI
+    {
+        private CourseController _controller;
+
+        public CourseUI(CourseController controller) => _controller = controller;
+
+        internal void OperationsWithCourses()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose operation:\r\n[1] create\r\n[2] edit\r\n[3] delete\r\n\r\n write \"back\" to step back");
+
+                try
+                {
+                    int option = ConsoleUI.GetIntInput();
+
+                    switch (option)
+                    {
+                        case 1:
+                            CreateCourse();
+                            break;
+                        case 2:
+                            EditCourse();
+                            break;
+                        case 3:
+                            DeleteCourse();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+
+        private void CreateCourse()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Write name of course\r\n\r\n write \"back\" to step back");
+
+                string name = Console.ReadLine();
+                if (name.Equals("back"))
+                    break;
+
+                int intCost = -1;
+                intCost = GetCost();
+
+                if (intCost == -1)
+                    break;
+
+                Course course = new Course();
+                course.Name = name;
+                course.Cost = intCost;
+                course.CreatedAt = DateTime.Now;
+
+                if (_controller.Create(course) == 1)
+                {
+                    Console.WriteLine("Operation is successfull. Press any key to continue...");
+                    if (Console.ReadKey().Equals(new ConsoleKeyInfo()))
+                        return;
+                }
+
+                return;
+            }
+        }
+
+        private int GetCost()
+        {
+            int cost = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Write cost of course\r\n\r\n write \"back\" to step back");
+
+                try
+                {
+                    cost = ConsoleUI.GetIntInput();
+                }
+                catch (ArgumentException)
+                {
+                    continue;
+                }
+                catch (Exception)
+                {
+                    return -1;
+                }
+
+                if (cost < 0)
+                    continue;
+
+                break;
+            }
+
+            return cost;
+        }
+
+        private void EditCourse()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter course id");
+
+                int courseId = ConsoleUI.GetIntInput();
+
+
+            }
+            
+        }
+
+        private void DeleteCourse()
+        {
+
+        }
+
+    }
+}
